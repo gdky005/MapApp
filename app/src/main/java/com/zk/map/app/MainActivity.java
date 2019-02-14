6f5200ecff6586e8ad0a657c389d73f8b0e7b566
qingw <qingw@jumei.com>
2019-02-14 21:08:58 +0800

    1. 获取定位成功。

--- app/src/main/java/com/zk/map/app/MainActivity.java
+++ app/src/main/java/com/zk/map/app/MainActivity.java
@@ -1,31 +1,38 @@
 package com.zk.map.app;
 
 import android.Manifest;
-import android.app.Notification;
-import android.app.PendingIntent;
-import android.content.Intent;
+import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
-import com.blankj.utilcode.util.ToastUtils;
+import com.baidu.location.Poi;
+import com.zk.map.app.service.LocationService;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LocationService locationService;
+    private TextView LocationResult;
+    private Button startLocation;
+    private String permissionInfo;
+
+
+
+
     private static final String TAG = "MainActivity";
+
+    private final int SDK_PERMISSION_REQUEST = 127;
 
     private static final int REQUEST_CODE_ACCESS_COARSE_LOCATION = 1;
 
@@ -38,17 +45,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    LocationResult.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-//                    mTextMessage.setText(R.string.title_dashboard);
-                    mTextMessage.setText("重新获取定位");
-                    ToastUtils.showShort("重新获取定位信息。");
-                    initLocationOption();
-
+                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    LocationResult.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -60,149 +63,238 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //Android 6.0 权限问题
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//如果 API level 是大于等于 23(Android 6.0) 时
-            //判断是否具有权限
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                //判断
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                    Toast.makeText(MainActivity.this,"KARL-Dujinyang 是否需要打开位置权限",Toast.LENGTH_SHORT).show();
-                }
-                //请求权限
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        REQUEST_CODE_ACCESS_COARSE_LOCATION);
-            }
-        }
-
-
-
-
-
-        initLocationOption();
-
-        mTextMessage = (TextView) findViewById(R.id.message);
+        LocationResult = (TextView) findViewById(R.id.message);
+        startLocation = (Button) findViewById(R.id.startLocation);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        getPersimmions();
     }
 
 
-    /**
-     * 初始化定位参数配置
-     */
-
-    private void initLocationOption() {
-//定位服务的客户端。宿主程序在客户端声明此类，并调用，目前只支持在主线程中启动
-        LocationClient locationClient = new LocationClient(getApplicationContext());
-//声明LocationClient类实例并配置定位参数
-        LocationClientOption locationOption = new LocationClientOption();
-        MyLocationListener myLocationListener = new MyLocationListener();
-//注册监听函数
-        locationClient.registerLocationListener(myLocationListener);
-//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-        locationOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
-//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-        locationOption.setCoorType("bd09ll");
-//可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
-        locationOption.setScanSpan(1000);
-//可选，设置是否需要地址信息，默认不需要
-        locationOption.setIsNeedAddress(true);
-//可选，设置是否需要地址描述
-        locationOption.setIsNeedLocationDescribe(true);
-//可选，设置是否需要设备方向结果
-        locationOption.setNeedDeviceDirect(false);
-//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
-        locationOption.setLocationNotify(true);
-//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
-        locationOption.setIgnoreKillProcess(true);
-//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
-        locationOption.setIsNeedLocationDescribe(true);
-//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-        locationOption.setIsNeedLocationPoiList(true);
-//可选，默认false，设置是否收集CRASH信息，默认收集
-        locationOption.SetIgnoreCacheException(false);
-//可选，默认false，设置是否开启Gps定位
-        locationOption.setOpenGps(true);
-//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
-        locationOption.setIsNeedAltitude(false);
-//设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK本身发现位置变化就会及时回调给开发者
-        locationOption.setOpenAutoNotifyMode();
-//设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者
-        locationOption.setOpenAutoNotifyMode(3000, 1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
-
-//        // TODO: 2019/2/14 待定是否有效。
-//        locationOption.setWifiCacheTimeOut(5*60*1000);
-//可选，V7.2版本新增能力
-//如果设置了该接口，首次启动定位时，会先判断当前Wi-Fi是否超出有效期，若超出有效期，会先重新扫描Wi-Fi，然后定位
-        
-        //开始定位
-        locationClient.start();
 
 
 
-
-        //开启前台定位服务：
-
-        Notification.Builder builder = new Notification.Builder (MainActivity.this.getApplicationContext());
-//获取一个Notification构造器
-
-        Intent nfIntent = new Intent(MainActivity.this.getApplicationContext(), MainActivity.class);
-        builder.setContentIntent(PendingIntent.getActivity(MainActivity.this, 0, nfIntent, 0)) // 设置PendingIntent
-                .setContentTitle("正在进行后台定位") // 设置下拉列表里的标题
-                .setSmallIcon(R.mipmap.ic_launcher) // 设置状态栏内的小图标
-                .setContentText("后台定位通知") // 设置上下文内容
-                .setAutoCancel(true)
-                .setWhen(System.currentTimeMillis()); // 设置该通知发生的时间
-        Notification notification = null;
-        notification = builder.build();
-        notification.defaults = Notification.DEFAULT_SOUND; //设置为默认的声音
-        locationClient.enableLocInForeground(1001, notification);// 调起前台定位
-    }
-
-    /**
-     * 实现定位回调
-     */
-    public class MyLocationListener extends BDAbstractLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
-            //以下只列举部分获取经纬度相关（常用）的结果信息
-            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
-
-            //获取纬度信息
-            double latitude = location.getLatitude();
-            //获取经度信息
-            double longitude = location.getLongitude();
-            //获取定位精度，默认值为0.0f
-            float radius = location.getRadius();
-            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
-            String coorType = location.getCoorType();
-            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
-            int errorCode = location.getLocType();
-
-
-            if ("4.9E-324".equals(String.valueOf(latitude))) {
-                Toast.makeText(MainActivity.this, "定位失败，请查看手机是否开启了定位权限", Toast.LENGTH_SHORT).show();
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            /*
+             * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+             */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            // 读取电话状态权限
+            if (addPermission(permissions, Manifest.permission.READ_PHONE_STATE)) {
+                permissionInfo += "Manifest.permission.READ_PHONE_STATE Deny \n";
             }
 
-
-            Log.d(TAG, "onReceiveLocation: "
-                    + latitude + ", "
-                    + longitude + ", "
-                    + radius + ", "
-                    + coorType + ", "
-                    + errorCode + ", "
-            );
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
         }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) { // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+            if (shouldShowRequestPermissionRationale(permission)){
+                return true;
+            }else{
+                permissionsList.add(permission);
+                return false;
+            }
+
+        }else{
+            return true;
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+
+
+    /***
+     * Stop location service
+     */
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        locationService.unregisterListener(mListener); //注销掉监听
+        locationService.stop(); //停止定位服务
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        // -----------location config ------------
+        locationService = ((MapApplication) getApplication()).locationService;
+        //获取locationservice实例，建议应用中只初始化1个location实例，然后使用，可以参考其他示例的activity，都是通过此种方式获取locationservice实例的
+        locationService.registerListener(mListener);
+        //注册监听
+        int type = getIntent().getIntExtra("from", 0);
+        if (type == 0) {
+            locationService.setLocationOption(locationService.getDefaultLocationClientOption());
+        } else if (type == 1) {
+            locationService.setLocationOption(locationService.getOption());
+        }
+        startLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (startLocation.getText().toString().equals(getString(R.string.startlocation))) {
+                    locationService.start();// 定位SDK
+                    // start之后会默认发起一次定位请求，开发者无须判断isstart并主动调用request
+                    startLocation.setText(getString(R.string.stoplocation));
+                } else {
+                    locationService.stop();
+                    startLocation.setText(getString(R.string.startlocation));
+                }
+            }
+        });
+    }
+
+
+    /*****
+     *
+     * 定位结果回调，重写onReceiveLocation方法，可以直接拷贝如下代码到自己工程中修改
+     *
+     */
+    private BDAbstractLocationListener mListener = new BDAbstractLocationListener() {
 
         @Override
-        public void onLocDiagnosticMessage(int i, int i1, String s) {
-            super.onLocDiagnosticMessage(i, i1, s);
+        public void onReceiveLocation(BDLocation location) {
+            // TODO Auto-generated method stub
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                StringBuffer sb = new StringBuffer(256);
+                sb.append("time : ");
+                /**
+                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
+                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
+                 */
+                sb.append(location.getTime());
+                sb.append("\nlocType : ");// 定位类型
+                sb.append(location.getLocType());
+                sb.append("\nlocType description : ");// *****对应的定位类型说明*****
+                sb.append(location.getLocTypeDescription());
+                sb.append("\nlatitude : ");// 纬度
+                sb.append(location.getLatitude());
+                sb.append("\nlontitude : ");// 经度
+                sb.append(location.getLongitude());
+                sb.append("\nradius : ");// 半径
+                sb.append(location.getRadius());
+                sb.append("\nCountryCode : ");// 国家码
+                sb.append(location.getCountryCode());
+                sb.append("\nCountry : ");// 国家名称
+                sb.append(location.getCountry());
+                sb.append("\ncitycode : ");// 城市编码
+                sb.append(location.getCityCode());
+                sb.append("\ncity : ");// 城市
+                sb.append(location.getCity());
+                sb.append("\nDistrict : ");// 区
+                sb.append(location.getDistrict());
+                sb.append("\nStreet : ");// 街道
+                sb.append(location.getStreet());
+                sb.append("\naddr : ");// 地址信息
+                sb.append(location.getAddrStr());
+                sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
+                sb.append(location.getUserIndoorState());
+                sb.append("\nDirection(not all devices have value): ");
+                sb.append(location.getDirection());// 方向
+                sb.append("\nlocationdescribe: ");
+                sb.append(location.getLocationDescribe());// 位置语义化信息
+                sb.append("\nPoi: ");// POI信息
+                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
+                    for (int i = 0; i < location.getPoiList().size(); i++) {
+                        Poi poi = (Poi) location.getPoiList().get(i);
+                        sb.append(poi.getName() + ";");
+                    }
+                }
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\nspeed : ");
+                    sb.append(location.getSpeed());// 速度 单位：km/h
+                    sb.append("\nsatellite : ");
+                    sb.append(location.getSatelliteNumber());// 卫星数目
+                    sb.append("\nheight : ");
+                    sb.append(location.getAltitude());// 海拔高度 单位：米
+                    sb.append("\ngps status : ");
+                    sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    // 运营商信息
+                    if (location.hasAltitude()) {// *****如果有海拔高度*****
+                        sb.append("\nheight : ");
+                        sb.append(location.getAltitude());// 单位：米
+                    }
+                    sb.append("\noperationers : ");// 运营商信息
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("离线定位成功，离线定位结果也是有效的");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+                logMsg(sb.toString());
+            }
+        }
 
-            Log.d(TAG, "onLocDiagnosticMessage: i==" + i + ", i1==" + i1 + ", s==" + s);
+    };
+
+    /**
+     * 显示请求字符串
+     *
+     * @param str
+     */
+    public void logMsg(String str) {
+        final String s = str;
+        try {
+            if (LocationResult != null){
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        LocationResult.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                LocationResult.setText(s);
+                            }
+                        });
+
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/zk/map/app/MapApplication.java
+++ app/src/main/java/com/zk/map/app/MapApplication.java
@@ -1,10 +1,18 @@
 package com.zk.map.app;
 
 import android.app.Application;
+import android.app.Service;
+import android.os.Vibrator;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.zk.map.app.service.LocationService;
 
 import team.zhuoke.sdk.ZKBase;
 
 public class MapApplication extends Application {
+
+    public LocationService locationService;
+    public Vibrator mVibrator;
 
     @Override
     public void onCreate() {
@@ -12,7 +20,14 @@ public class MapApplication extends Application {
 
         ZKBase.init(this);
 
-
+        /***
+         * 初始化定位sdk，建议在Application中创建
+         */
+        locationService = new LocationService(getApplicationContext());
+        mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
+        SDKInitializer.initialize(getApplicationContext());
 //        SuITjygZLWqSDXNOznQt5x2dRegws5Kt
+
+
     }
 }
