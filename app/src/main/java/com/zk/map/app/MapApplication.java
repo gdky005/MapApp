a54aaa14311791b402d8135418e0c7355c9b1c19
qingw <qingw@jumei.com>
2019-02-14 19:12:10 +0800

    1. 初始化 map app 的项目； 2. 添加百度 SDK 的基础功能。

---
+++ app/src/main/java/com/zk/map/app/MapApplication.java
@@ -0,0 +1,13 @@
+package com.zk.map.app;
+
+import android.app.Application;
+
+public class MapApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        SuITjygZLWqSDXNOznQt5x2dRegws5Kt
+    }
+}
